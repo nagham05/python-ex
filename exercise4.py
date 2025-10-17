dae6c10006a82b7798716b9e03ad9abb85a22b71
nagham05 <naghamhabli89@gmail.com>
2025-10-17 11:09:17 +0300

    additional note

--- exercise4.py
+++ exercise4.py
@@ -1,4 +1,5 @@
 def check_num(nums,x):
+    # print(x in nums) this prints True or False but thre funstions should return it not print so we use return 
     return x in nums
 
 numbers = [1, 3, 5, 7, 9]
